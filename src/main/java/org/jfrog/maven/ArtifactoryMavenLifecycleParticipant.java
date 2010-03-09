291fabc55c928e503a9269d4aaaca80d5b5272e5
noamt <noamt@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-03-09 16:11:46 +0000

    RTFACT-2798: - Gather basic maven build info - Add logger - Add commons lang dep (needed for build info api)

--- src/main/java/org/jfrog/maven/ArtifactoryMavenLifecycleParticipant.java
+++ src/main/java/org/jfrog/maven/ArtifactoryMavenLifecycleParticipant.java
@@ -5,6 +5,8 @@ import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.ExecutionListener;
 import org.apache.maven.execution.MavenSession;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
 
 /**
  * @author Noam Y. Tenne
@@ -12,11 +14,14 @@ import org.codehaus.plexus.component.annotations.Component;
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public class ArtifactoryMavenLifecycleParticipant extends AbstractMavenLifecycleParticipant {
 
+    @Requirement
+    private Logger logger;
+
     @Override
     public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
         ExecutionListener existingExecutionListener = session.getRequest().getExecutionListener();
         ArtifactoryExecutionListener artifactoryExecutionListener =
-                new ArtifactoryExecutionListener(existingExecutionListener);
+                new ArtifactoryExecutionListener(logger, existingExecutionListener);
         session.getRequest().setExecutionListener(artifactoryExecutionListener);
     }
 }
