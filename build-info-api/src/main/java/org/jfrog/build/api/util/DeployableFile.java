aa8ce740e4375c49731c44b259cf13d90e3a2162
noamt <noamt@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-04-18 14:03:33 +0000

    Add GAVC to deployable file

--- build-info-api/src/main/java/org/jfrog/build/api/util/DeployableFile.java
+++ build-info-api/src/main/java/org/jfrog/build/api/util/DeployableFile.java
@@ -22,16 +22,56 @@ public class DeployableFile extends BaseBuildFileBean {
      */
     File file;
 
-    public DeployableFile(BuildFileBean buildFile, File file) {
-        this.buildFile = buildFile;
-        this.file = file;
-    }
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String classifier;
 
     public BuildFileBean getBuildFile() {
         return buildFile;
     }
 
+    public void setBuildFile(BuildFileBean buildFile) {
+        this.buildFile = buildFile;
+    }
+
     public File getFile() {
         return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getClassifier() {
+        return classifier;
+    }
+
+    public void setClassifier(String classifier) {
+        this.classifier = classifier;
     }
 }
