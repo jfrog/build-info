df84fcc419e1a30b00e2f534d56391d01c31cbb5
tomerc <tomerc@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-06-30 14:44:04 +0000

    IAP-1: Generic Ivy extractor

--- src/main/java/org/jfrog/build/extractor/trigger/IvyDependencyTrigger.java
+++ src/main/java/org/jfrog/build/extractor/trigger/IvyDependencyTrigger.java
@@ -1,4 +1,4 @@
-package org.jfrog.build.extractor.listener;
+package org.jfrog.build.extractor.trigger;
 
 import com.google.common.collect.Lists;
 import org.apache.ivy.core.event.IvyEvent;
--- src/main/java/org/jfrog/build/extractor/trigger/IvyModuleTrigger.java
+++ src/main/java/org/jfrog/build/extractor/trigger/IvyModuleTrigger.java
@@ -1,4 +1,4 @@
-package org.jfrog.build.extractor.listener;
+package org.jfrog.build.extractor.trigger;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
