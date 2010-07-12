cd8dbb960cbb740740f6a965d8767c8021dfafc8
tomerc <tomerc@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-07-12 15:26:06 +0000

    IAP-1: Generic Ivy extractor

--- src/main/java/org/jfrog/build/extractor/task/ArtifactoryPublishTask.java
+++ src/main/java/org/jfrog/build/extractor/task/ArtifactoryPublishTask.java
@@ -1,15 +1,19 @@
 package org.jfrog.build.extractor.task;
 
-import org.apache.ivy.ant.IvyDeliver;
 import org.apache.ivy.ant.IvyPublish;
+import org.apache.ivy.ant.IvyTask;
 import org.apache.ivy.core.IvyContext;
 import org.apache.ivy.core.settings.IvySettings;
 import org.apache.ivy.plugins.repository.Repository;
 import org.apache.ivy.plugins.repository.url.URLRepository;
 import org.apache.ivy.plugins.resolver.DependencyResolver;
-import org.apache.ivy.plugins.resolver.IBiblioResolver;
 import org.apache.ivy.plugins.resolver.URLResolver;
+import org.apache.ivy.util.url.CredentialsStore;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.PropertyHelper;
+
+import java.util.List;
 
 /**
  * A customized Ivy task that is used to configure a custom resolver which wraps around the existing resolver. It
@@ -18,19 +22,23 @@ import org.apache.tools.ant.BuildException;
  *
  * @author Tomer Cohen
  */
-public class ArtifactoryPublishTask extends IvyDeliver {
+public class ArtifactoryPublishTask extends IvyPublish {
     private static final String RESOLVER_NAME = "artifactory_resolver";
     private static final String REPOSITORY_NAME = "artifactory";
+    private static final String DEFAULT_REALM = "Artifactory Realm";
     public static final String PUBLISH_ARTIFACT_TASK_NAME = "publish_artifact";
 
     @Override
     public void doExecute() throws BuildException {
-        IvyContext ivyContext = IvyContext.getContext();
-        IvySettings ivySettings = ivyContext.getSettings();
+        IvySettings ivySettings = getSettings();
         if (ivySettings.getResolver(RESOLVER_NAME) == null) {
             ivySettings.addResolver(createArtifactoryDependencyResolver(ivySettings));
         }
-        setDeliverpattern(IBiblioResolver.DEFAULT_PATTERN);
+        Project proj = (Project) IvyContext.peekInContextStack(IvyTask.ANT_PROJECT_CONTEXT_KEY);
+        List list = (List) PropertyHelper.getPropertyHelper(proj).getUserProperty("artifactory.ant.context");
+        //setArtifactspattern(IBiblioResolver.DEFAULT_PATTERN);
+        CredentialsStore.INSTANCE.addCredentials(DEFAULT_REALM, "", "", "");
+        super.doExecute();
     }
 
     private DependencyResolver createArtifactoryDependencyResolver(IvySettings ivySettings) {
