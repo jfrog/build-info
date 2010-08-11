85bd64922c0b20de5bd78d378bc433aed93c63fa
noamt <noamt@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-08-11 06:57:33 +0000

    MAP-9 - Attached artifacts are no longer being collected

--- build-info-extractor-maven3/src/main/java/org/jfrog/build/extractor/maven/BuildInfoRecorder.java
+++ build-info-extractor-maven3/src/main/java/org/jfrog/build/extractor/maven/BuildInfoRecorder.java
@@ -21,13 +21,10 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.metadata.ArtifactMetadata;
 import org.apache.maven.execution.ExecutionEvent;
 import org.apache.maven.execution.ExecutionListener;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.Model;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.logging.Logger;
@@ -277,7 +274,7 @@ public class BuildInfoRecorder implements BuildInfoExtractor<ExecutionEvent, Bui
             logger.warn("Skipping Artifactory Build-Info dependency extraction: Null project.");
             return;
         }
-        extractModuleDependencies(project);
+        extractArtifactsAndDependencies(project);
 
         if (wrappedListener != null) {
             wrappedListener.mojoSucceeded(event);
@@ -400,23 +397,16 @@ public class BuildInfoRecorder implements BuildInfoExtractor<ExecutionEvent, Bui
             }
 
             if (!isPomProject(moduleArtifact)) {
-                for (ArtifactMetadata metadata : moduleArtifact.getMetadataList()) {
-                    if (metadata instanceof ProjectArtifactMetadata) {
-                        Model model = project.getModel();
-                        File pomFile = null;
-                        if (model != null) {
-                            pomFile = model.getPomFile();
-                        }
-                        artifactBuilder.type("pom");
-                        artifactBuilder.name(artifactName.replace(artifactExtension, "pom"));
-                        org.jfrog.build.api.Artifact pomArtifact = artifactBuilder.build();
-                        currentModule.addArtifact(pomArtifact);
-                        if (pomFile != null && pomFile.isFile() && isPublishArtifacts()) {
-                            addDeployableArtifact(pomArtifact, pomFile, moduleArtifact.getGroupId(),
-                                    artifactId, artifactVersion,
-                                    artifactClassifier, "pom");
-                        }
-                    }
+                artifactBuilder.type("pom");
+                artifactBuilder.name(artifactName.replace(artifactExtension, "pom"));
+                org.jfrog.build.api.Artifact pomArtifact = artifactBuilder.build();
+                currentModule.addArtifact(pomArtifact);
+
+                File pomFile = project.getFile();
+                if (pomFile != null && pomFile.isFile() && isPublishArtifacts()) {
+                    addDeployableArtifact(pomArtifact, pomFile, moduleArtifact.getGroupId(),
+                            artifactId, artifactVersion,
+                            artifactClassifier, "pom");
                 }
             }
         }
