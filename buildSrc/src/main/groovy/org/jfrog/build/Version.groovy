41f289d0f96dfd187a8bacf0fa02d64fff00f28e
freds <freds@c2c9f8a3-3e2a-0410-aff6-b45ccb734f43>
2010-12-12 13:47:01 +0000

    Version system

--- buildSrc/src/main/groovy/org/jfrog/build/Version.groovy
+++ buildSrc/src/main/groovy/org/jfrog/build/Version.groovy
@@ -1,61 +1,77 @@
 package org.jfrog.build
 
-import java.text.SimpleDateFormat
 import org.gradle.api.GradleException
 import org.gradle.api.Project
+import java.text.SimpleDateFormat
 
 class Version {
-  String versionNumber
-  Date buildTime
-  Boolean release = null
+    String versionNumber
+    Date buildTime
+    Boolean release = null
 
-  def Version(Project project) {
-    this.versionNumber = project.nextVersion
-    this.release = Boolean.parseBoolean(project.isRelease)
-    File timestampFile = new File(project.buildDir, 'timestamp.txt')
-    if (timestampFile.isFile()) {
-      boolean uptodate = true
-      def modified = timestampFile.lastModified()
-      project.fileTree('src/main').visit {fte ->
-        if (fte.file.isFile() && fte.lastModified > modified) {
-          uptodate = false
-          fte.stopVisiting()
-        }
-      }
-      if (!uptodate) {
-        timestampFile.setLastModified(new Date().time)
-      }
-    } else {
-      timestampFile.parentFile.mkdirs()
-      timestampFile.createNewFile()
+    def Version(Project project, String versionNumber, boolean isRelease) {
+        this(project, versionNumber, isRelease, null)
     }
-    buildTime = new Date(timestampFile.lastModified())
-    if (!release)
-      this.versionNumber += "-" + getTimestamp()
-    /*
-            project.gradle.taskGraph.whenReady {graph ->
-                if (graph.hasTask(':releaseVersion')) {
-                    release = true
-                } else {
-                    this.versionNumber += "-" + getTimestamp()
-                    release = false
+
+    def Version(Project project, String versionNumber, boolean isRelease, List<String> subProjects) {
+        this.versionNumber = versionNumber
+        this.release = isRelease
+        File timestampFile = new File(project.buildDir, 'timestamp.txt')
+        if (timestampFile.isFile()) {
+            boolean uptodate = true
+            def modified = timestampFile.lastModified()
+            if (subProjects != null) {
+                // Check timestamp by list of subprojects
+                subProjects.each { spName ->
+                    project.project(spName).fileTree('src/main').visit {fte ->
+                        if (fte.file.isFile() && fte.lastModified > modified) {
+                            uptodate = false
+                            fte.stopVisiting()
+                        }
+                    }
+                }
+            } else {
+                project.fileTree('src/main').visit {fte ->
+                    if (fte.file.isFile() && fte.lastModified > modified) {
+                        uptodate = false
+                        fte.stopVisiting()
+                    }
                 }
             }
-    */
-  }
-
-  String toString() {
-    versionNumber
-  }
-
-  String getTimestamp() {
-    new SimpleDateFormat('yyyyMMddHHmmssZ').format(buildTime)
-  }
-
-  boolean isRelease() {
-    if (release == null) {
-      throw new GradleException("Can't determine whether this is a release build before the task graph is populated")
+            if (!uptodate) {
+                timestampFile.setLastModified(new Date().time)
+            }
+        } else {
+            timestampFile.parentFile.mkdirs()
+            timestampFile.createNewFile()
+        }
+        buildTime = new Date(timestampFile.lastModified())
+        if (!release)
+            this.versionNumber += "-" + getTimestamp()
+        /*
+                project.gradle.taskGraph.whenReady {graph ->
+                    if (graph.hasTask(':releaseVersion')) {
+                        release = true
+                    } else {
+                        this.versionNumber += "-" + getTimestamp()
+                        release = false
+                    }
+                }
+        */
     }
-    return release
-  }
+
+    String toString() {
+        versionNumber
+    }
+
+    String getTimestamp() {
+        new SimpleDateFormat('yyyyMMddHHmmssZ').format(buildTime)
+    }
+
+    boolean isRelease() {
+        if (release == null) {
+            throw new GradleException("Can't determine whether this is a release build before the task graph is populated")
+        }
+        return release
+    }
 }
