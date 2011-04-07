f9d2d1b8cb5ceb53a00830d8db538f9da5a95db5
Tomer Cohen <tomerc@jfrog.com>
2011-04-07 13:57:55 +0300

    use versioning system by analyzing the version string

--- buildSrc/src/main/groovy/org/jfrog/build/Version.groovy
+++ buildSrc/src/main/groovy/org/jfrog/build/Version.groovy
@@ -1,13 +1,38 @@
+/*
+ *
+ *  Copyright (C) 2010 JFrog Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+
 package org.jfrog.build
 
 import org.gradle.api.GradleException
 import org.gradle.api.Project
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 
 class Version {
     String versionNumber
     Date buildTime
     Boolean release = null
+    int indexOfSnapshot
 
     def Version(Project project) {
         this(project, null)
@@ -15,7 +40,11 @@ class Version {
 
     def Version(Project project, List<String> subProjects) {
         this.versionNumber = project.getProperty("${project.name}-version")
-        this.release = Boolean.valueOf(project.getProperty("${project.name}-release"))
+        this.release = true
+        if (versionNumber.contains('SNAPSHOT')) {
+            this.release = false
+            this.indexOfSnapshot = this.versionNumber.indexOf('SNAPSHOT')
+        }
         File timestampFile = new File(project.buildDir, 'timestamp.txt')
         if (timestampFile.isFile()) {
             boolean uptodate = true
@@ -47,27 +76,55 @@ class Version {
         }
         buildTime = new Date(timestampFile.lastModified())
         if (!release) {
-            //def ts = new SimpleDateFormat('yyyyMMddHHmmss').format(buildTime)
-            this.versionNumber += "-SNAPSHOT" // + ts
-            /*
-                    project.gradle.taskGraph.whenReady {graph ->
-                        if (graph.hasTask(':releaseVersion')) {
-                            release = true
-                        } else {
-                            this.versionNumber += "-" + getTimestamp()
-                            release = false
-                        }
-                    }
-            */
+            this.versionNumber = this.versionNumber.substring(0, indexOfSnapshot - 1)
+            this.versionNumber += "-" + getTimestamp()
         }
-    }
-
-    String getStatus() {
-        if (release) return 'release'
-        return 'integration'
     }
 
     String toString() {
         versionNumber
+    }
+
+    String getTimestamp() {
+        // Convert local file timestamp to UTC
+        def format = new SimpleDateFormat('yyyyMMddHHmmss')
+        format.setCalendar(Calendar.getInstance(TimeZone.getTimeZone('UTC')));
+        return format.format(buildTime)
+    }
+
+    String getStatus() {
+        return release ? "release" : "integration"
+    }
+
+    boolean isRelease() {
+        if (release == null) {
+            throw new GradleException("Can't determine whether this is a release build before the task graph is populated")
+        }
+        return release
+    }
+}
+
+class WriteVersionProperties extends DefaultTask {
+    @Input
+    String getVersion() { return project.version.toString() }
+
+    @Input
+    Date getBuildTime() { return project.version.buildTime }
+
+    @OutputFile
+    File propertiesFile
+
+    @TaskAction
+    def void generate() {
+        logger.info('Write version properties to: {}', propertiesFile)
+        Properties versionProperties = new Properties()
+        versionProperties.putAll([
+                'version': version,
+                'buildTime': DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL).format(buildTime)
+        ])
+        propertiesFile.withOutputStream {
+            versionProperties.store(it, '')
+        }
+
     }
 }
