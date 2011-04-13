e2b519babd15f5a8a13cc87dcbf13377b9e1cb56
Tomer Cohen <tomerc@jfrog.com>
2011-04-13 15:41:37 +0300

    enhance DSL add copyright license

--- build-info-extractor-gradle/src/main/groovy/org/jfrog/dsl/ArtifactoryPluginConvention.groovy
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/dsl/ArtifactoryPluginConvention.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2010 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jfrog.dsl
 
 import com.google.common.collect.Lists
@@ -31,7 +47,7 @@ class ArtifactoryPluginConvention {
     }
 
     def publish(Closure closure) {
-        ConfigureUtil.configure(closure, configuration.publisher)
+        new PublisherConfig(configuration.publisher).config(closure)
     }
 
     def resolve(Closure closure) {
---
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/dsl/PublisherConfig.groovy
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2010 JFrog Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jfrog.dsl
+
+import org.gradle.util.ConfigureUtil
+import org.jfrog.build.client.ArtifactoryClientConfiguration.PublisherHandler
+
+/**
+ * @author Tomer Cohen
+ */
+class PublisherConfig {
+
+    private PublisherHandler handler;
+    private Repository repository;
+
+    PublisherConfig(PublisherHandler handler) {
+        this.handler = handler
+        this.repository = new Repository()
+    }
+
+    def methodMissing(String name, args) {
+        //println "1: missing method $name"
+        ConfigureUtil.configure(args[0], handler)
+    }
+
+    def propertyMissing(String name, value) {
+        handler[name] = value
+    }
+
+
+    def config(Closure closure) {
+        ConfigureUtil.configure(closure, this)
+    }
+
+
+    def setBuildInfo(boolean buildinfo) {
+        handler.setPublishBuildInfo(buildinfo)
+    }
+
+    def setPublishPom(boolean publishPom) {
+        handler.setMaven(publishPom)
+    }
+
+    def setPublishIvy(boolean publishIvy) {
+        handler.setIvy(publishIvy)
+    }
+
+    def repository(Closure closure) {
+        ConfigureUtil.configure(closure, repository)
+    }
+
+    public class Repository {
+
+        def setUsername(String username) {
+            handler.setUserName(username)
+        }
+
+        def setPassword(String password) {
+            handler.setPassword(password)
+        }
+
+        def setIvyLayout(String ivyLayout) {
+            handler.setIvyPattern(ivyLayout)
+        }
+
+        def setArtifactLayout(String artifactLayout) {
+            handler.setIvyArtifactPattern(artifactLayout)
+        }
+
+        def setUrl(String url) {
+            handler.setUrl(url)
+        }
+
+        def ivy(Closure closure) {
+            ConfigureUtil.configure(closure, this)
+        }
+    }
+}
+
+
