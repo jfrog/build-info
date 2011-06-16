0c79a2a3f859c0d3180e5ad59864e06969071d85
Tomer Cohen <tomerc@jfrog.com>
2011-06-16 15:15:44 +0300

    reactivate test

--- build-info-extractor-gradle/src/test/groovy/org/jfrog/build/ArtifactoryPluginTest.groovy
+++ build-info-extractor-gradle/src/test/groovy/org/jfrog/build/ArtifactoryPluginTest.groovy
@@ -109,7 +109,7 @@ public class ArtifactoryPluginTest extends Specification {
         buildInfoTask.dependsOn.size() == 1
     }
 
-    /* def populateConfigurationFromDsl() {
+    def populateConfigurationFromDsl() {
         URL resource = getClass().getResource('/org/jfrog/build/build.gradle')
         def projDir = new File(resource.toURI()).getParentFile()
 
@@ -136,7 +136,7 @@ public class ArtifactoryPluginTest extends Specification {
         'p33p' == clientConfig.publisher.password
         //Cannot call clientConfig.publisher.isMaven() since it is only assigned at task execution
         !buildInfoTask.getPublishPom()
-    }*/
+    }
 
     private def projectEvaluated(Project project) {
         BuildListener next = project.getGradle().listenerManager.allListeners.iterator().next()
