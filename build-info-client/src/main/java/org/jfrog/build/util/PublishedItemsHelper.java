88a709f2d35f9c5829a65edc8f221c1fbe6fabed
Shay Yaakov <shayy@jfrog.org>
2012-03-11 20:41:44 +0200

    BI-122 - Add new module for VCS manipulations A generic post-build action to publish artifacts to Artifactory, added support for absolute path as well as relative path to the workspace (../dir)

--- build-info-client/src/main/java/org/jfrog/build/util/PublishedItemsHelper.java
+++ build-info-client/src/main/java/org/jfrog/build/util/PublishedItemsHelper.java
@@ -31,10 +31,22 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
+ * Helper class for calculating published artifacts of a generic deployment.
+ *
  * @author Noam Y. Tenne
  */
 public class PublishedItemsHelper {
 
+    /**
+     * Splits a given property value to pairs of source and target strings (the splitter is '=>'
+     * the source represents the Ant Pattern to search for
+     * the target represents the target path to deploy the found artifacts
+     * Multi values as acceptable by new lined or comma separated.
+     *
+     * @param publishedItemsPropertyValue the string value to split, if the splitter '=>' was not found
+     *                                    then the value is treated as a source only (target will be "").
+     * @return a Map containing the sources as keys and targets as values
+     */
     public static Map<String, String> getPublishedItemsPatternPairs(String publishedItemsPropertyValue) {
         Map<String, String> patternPairMap = Maps.newHashMap();
         if (StringUtils.isNotBlank(publishedItemsPropertyValue)) {
@@ -50,6 +62,8 @@ public class PublishedItemsHelper {
                 if (splitPattern.length > 0) {
                     sourcePattern = FilenameUtils.separatorsToUnix(splitPattern[0].trim());
                 }
+
+                // We allow an empty target, in that case it will be ""
                 if (splitPattern.length > 1) {
                     targetPattern = FilenameUtils.separatorsToUnix(splitPattern[1].trim());
                 }
@@ -62,8 +76,13 @@ public class PublishedItemsHelper {
         return patternPairMap;
     }
 
-    public static List<String> parsePatternsFromProperty(String publishedItemsPropertyValue) {
-
+    /**
+     * Splits the given property value by new lines or by commas.
+     *
+     * @param publishedItemsPropertyValue The property value to split
+     * @return a List of the splinted parameter by new lines or commas.
+     */
+    private static List<String> parsePatternsFromProperty(String publishedItemsPropertyValue) {
         if (publishedItemsPropertyValue == null) {
             throw new IllegalArgumentException("Cannot parse null pattern.");
         }
@@ -121,33 +140,56 @@ public class PublishedItemsHelper {
         return StringUtils.removeEnd(patternBuilder.toString(), "/");
     }
 
-    public static Multimap<String, File> buildPublishingData(File checkoutDir, String sourcePath, String targetPath)
+    /**
+     * Building a multi map of target paths mapped to their files.
+     *
+     * @param checkoutDir the base directory of which to calculate the given source ant pattern
+     * @param pattern     the Ant pattern to calculate the files from
+     * @param targetPath  the target path for deployment of a file
+     * @return a Multimap containing the targets as keys and the files as values
+     * @throws IOException in case of any file system exception
+     */
+    public static Multimap<String, File> buildPublishingData(File checkoutDir, String pattern, String targetPath)
             throws IOException {
         final Multimap<String, File> filePathsMap = HashMultimap.create();
-        File file = resolvePath(checkoutDir, sourcePath);
-        if (file.isFile()) {
-            filePathsMap.put(targetPath, file);
+        File patternAbsolutePath = getAbsolutePath(checkoutDir, pattern);
+        if (patternAbsolutePath.isFile()) {
+            // The given pattern is an absolute path of just one file, let's add it to our result map
+            filePathsMap.put(targetPath, patternAbsolutePath);
         } else {
             Pattern filePattern = null;
             File patternDir = null;
-            if (file.isDirectory()) {
+            if (patternAbsolutePath.isDirectory()) {
+                // The given pattern is a path to a directory, we need to return all it's content
                 filePattern = Pattern.compile(".*");
-                patternDir = file;
-            } else if (sourcePath.indexOf('*') >= 0 || sourcePath.indexOf('?') >= 0) {
-                File baseTruncationDir = getBaseTruncationDir(file);
+                patternDir = patternAbsolutePath;
+            } else if (pattern.indexOf('*') >= 0 || pattern.indexOf('?') >= 0) {
+                // We are dealing with complex Ant pattern, need to analyze it
+                File baseTruncationDir = getBaseTruncationDir(patternAbsolutePath);
                 patternDir = baseTruncationDir != null ? baseTruncationDir : checkoutDir;
-                boolean fileWithinCwd = isAncestor(checkoutDir, file);
 
-                if (fileWithinCwd) {
-                    sourcePath = sourcePath.substring(
+                // If the checkout dir is an ancestor of the pattern path then
+                // we cut the checkout dir from the pattern
+                boolean isCheckoutDirAncestor = isAncestor(checkoutDir, patternAbsolutePath);
+                if (isCheckoutDirAncestor) {
+                    pattern = pattern.substring(
                             patternDir.getAbsolutePath().length() - checkoutDir.getAbsolutePath().length());
                 }
-                /*                    if (sourcePath.startsWith(FileUtil.toSystemIndependentName(myBasePatternDir.getAbsolutePath()))) {
-                    path = FileUtil.getRelativePath(myBasePatternDir, new File(path));
-                    path = FileUtils
-                }*/
-                filePattern = Pattern.compile(convertAntToRegexp(sourcePath));
+
+                // If the pattern absolute path starts with the pattern directory path
+                // then we need to cut the pattern to be only the relative path
+                String patternAbsolutePathUrl = patternAbsolutePath.getAbsolutePath();
+                if (!StringUtils.isBlank(patternAbsolutePathUrl) && patternAbsolutePathUrl.startsWith(
+                        patternDir.getAbsolutePath())) {
+                    pattern = getRelativePath(patternDir, patternAbsolutePath);
+                }
+
+                // All done, we can now convert and compile from Ant pattern to regular expression
+                filePattern = Pattern.compile(convertAntToRegexp(pattern));
             }
+
+            // If we successfully converted from Ant pattern to a regular expression
+            // then it's time to collect all our artifacts according to this regular expression
             if (filePattern != null) {
                 List<File> files = new ArrayList<File>();
                 collectMatchedFiles(patternDir, patternDir, filePattern, files);
@@ -158,13 +200,20 @@ public class PublishedItemsHelper {
         return filePathsMap;
     }
 
-    private static File resolvePath(File baseDir, String path) {
-        File pathFile = new File(path);
+    /**
+     * Calculates the absolute path of the given Ant pattern relatively to the given base directory
+     *
+     * @param baseDir the base directory to use in case the pattern is not absolute
+     * @param pattern the pattern to which calculate an absolute path
+     * @return File representation of the calculated absolute path
+     */
+    private static File getAbsolutePath(File baseDir, String pattern) {
+        File pathFile = new File(pattern);
         if (pathFile.isAbsolute()) {
             return pathFile;
         }
 
-        File rawFile = new File(baseDir, path);
+        File rawFile = new File(baseDir, pattern);
         if (baseDir.getPath().startsWith("\\\\")) {
             return rawFile;
         }
@@ -172,8 +221,15 @@ public class PublishedItemsHelper {
         return new File(rawFile.toURI().normalize().getPath());
     }
 
-    private static File getBaseTruncationDir(File file) {
-        String dirWithoutPattern = getDirWithoutPattern(file.getPath());
+    /**
+     * Gets the base directory of a given File representation of an Ant pattern
+     * without the pattern itself
+     *
+     * @param antPatternDir the Ant pattern directory to calculate the base directory from
+     * @return the base directory of the given Ant pattern, null if there isn't any
+     */
+    private static File getBaseTruncationDir(File antPatternDir) {
+        String dirWithoutPattern = getDirWithoutPattern(antPatternDir.getPath());
         if ("".equals(dirWithoutPattern)) {
             return null;
         } else {
@@ -181,18 +237,43 @@ public class PublishedItemsHelper {
         }
     }
 
+    /**
+     * Returns the directory path of the given Ant pattern without the pattern itself.
+     *
+     * @param pathWithWildCard an Ant pattern containing wildcards
+     * @return the path to the directory without the Ant pattern
+     */
     private static String getDirWithoutPattern(String pathWithWildCard) {
         String t = pathWithWildCard.replace('\\', '/');
         int firstStar = t.indexOf('*');
-        int firstQuest = t.indexOf('?');
-        int mark = firstStar >= 0 ? firstStar >= firstQuest && firstQuest >= 0 ? firstQuest : firstStar : firstQuest;
+        int firstQuestion = t.indexOf('?');
+        int mark;
+        if (firstStar >= 0) {
+            if (firstStar >= firstQuestion && firstQuestion >= 0) {
+
+                mark = firstQuestion;
+            } else {
+                mark = firstStar;
+            }
+        } else {
+            mark = firstQuestion;
+        }
+
         int lastSlash = t.lastIndexOf('/', mark);
         return lastSlash <= 0 ? "" : pathWithWildCard.substring(0, lastSlash);
     }
 
-    private static boolean isAncestor(File ancestor, File file)
-            throws IOException {
-        File parent = file;
+    /**
+     * Checks if a given directory is an ansector (file system speaking)
+     * of a certain directory
+     *
+     * @param ancestor The directory to check if it's an ancestor of
+     * @param child    The child directory
+     * @return true if ancestor if an ancestor of child, false otherwise
+     * @throws IOException
+     */
+    private static boolean isAncestor(File ancestor, File child) throws IOException {
+        File parent = child;
         do {
             if (parent == null) {
                 return false;
@@ -298,6 +379,13 @@ public class PublishedItemsHelper {
         }
     }
 
+    /**
+     * Gets the relative path of a given file to the base
+     *
+     * @param base the base path to calculate the relative path
+     * @param file the file itself
+     * @return the calculated relative path
+     */
     private static String getRelativePath(File base, File file) {
         if (base == null || file == null) {
             return null;
@@ -318,19 +406,11 @@ public class PublishedItemsHelper {
     }
 
     private static String getRelativePath(String basePath, String filePath, char separator) {
-        return getRelativePath(basePath, filePath, separator, false);
-    }
-
-    private static String ensureEnds(String s, char endsWith) {
-        return StringUtils.endsWith(s, "/") ? s : (new StringBuilder()).append(s).append(endsWith).toString();
-    }
-
-    private static String getRelativePath(String basePath, String filePath, char separator, boolean caseSensitive) {
         basePath = ensureEnds(basePath, separator);
         int len = 0;
         int lastSeparatorIndex = 0;
-        String basePathToCompare = caseSensitive ? basePath : basePath.toLowerCase();
-        String filePathToCompare = caseSensitive ? filePath : filePath.toLowerCase();
+        String basePathToCompare = basePath.toLowerCase();
+        String filePathToCompare = filePath.toLowerCase();
         if (basePathToCompare.equals(ensureEnds(filePathToCompare, separator))) {
             return ".";
         }
@@ -356,7 +436,20 @@ public class PublishedItemsHelper {
         return relativePath.toString();
     }
 
-    public static String calculateTargetPath(String relativePath, String targetPattern, String fileName) {
+    private static String ensureEnds(String s, char endsWith) {
+        return StringUtils.endsWith(s, "/") ? s : (new StringBuilder()).append(s).append(endsWith).toString();
+    }
+
+    /**
+     * Calculates the target deployment path of an artifact by it's name
+     *
+     * @param targetPattern an Ant pattern of the target path
+     * @param artifactFile  the artifact file to calculate target deployment path for
+     * @param rootDir       the root dir to calculate relativity to
+     * @return the calculated target path
+     */
+    public static String calculateTargetPath(String targetPattern, File artifactFile, String rootDir) {
+        String relativePath = calculateTargetRelativePath(artifactFile, rootDir);
         if (relativePath == null) {
             throw new IllegalArgumentException("Cannot calculate a target path given a null relative path.");
         }
@@ -370,7 +463,7 @@ public class PublishedItemsHelper {
 
         // take care of absolute path
         if (StringUtils.startsWith(targetPattern, "/")) {
-            return targetPattern + "/" + fileName;
+            return targetPattern + "/" + artifactFile.getName();
         }
 
         // take care of relative paths with patterns.
@@ -420,5 +513,21 @@ public class PublishedItemsHelper {
             }
         }
         return itemPathBuilder.toString();
+    }
+
+    private static String calculateTargetRelativePath(File artifactFile, String rootDir) {
+        String relativePath = artifactFile.getAbsolutePath();
+        if (StringUtils.startsWith(relativePath, rootDir)) {
+            relativePath = StringUtils.removeStart(artifactFile.getAbsolutePath(), rootDir);
+        } else {
+            String parentDir = artifactFile.getParent();
+            if (!StringUtils.isBlank(parentDir)) {
+                relativePath = StringUtils.removeStart(artifactFile.getAbsolutePath(),
+                        parentDir);
+            }
+        }
+        relativePath = FilenameUtils.separatorsToUnix(relativePath);
+        relativePath = StringUtils.removeStart(relativePath, "/");
+        return relativePath;
     }
 }
