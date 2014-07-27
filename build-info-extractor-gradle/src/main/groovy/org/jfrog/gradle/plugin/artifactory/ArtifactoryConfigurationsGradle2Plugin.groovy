ed671f3816040a25dbe374a755cb04e6462ce143
eyalbe4 <eyalbenmoshe@jfrog.com>
2014-07-27 15:57:57 +0300

    GAP-153 - compatibility with Gradle 2.0 Fixed the tests to be compatible with the new plugin IDs

--- build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/ArtifactoryConfigurationsGradle2Plugin.groovy
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/ArtifactoryConfigurationsGradle2Plugin.groovy
@@ -1,7 +1,7 @@
 package org.jfrog.gradle.plugin.artifactory
 
 /**
- * This class links to "com.jfrog.artifactory-publish.properties" file -> " apply plugin: 'artifactory' "
+ * This class links to "com.jfrog.artifactory-upload.properties" file -> " apply plugin: 'com.jfrog.artifactory-upload' "
  *
  * @author Lior Hasson  
  */
