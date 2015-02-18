49e3852baa9283df0d33bbfa72ac59ad85456f24
shikloshi <aviads87@gmail.com>
2015-02-18 12:12:41 +0200

    BI-194 - Adding "publishToBintray" support to BI

---
+++ build-info-api/src/main/java/org/jfrog/build/api/release/BintrayUploadInfoOverride.java
@@ -0,0 +1,46 @@
+package org.jfrog.build.api.release;
+
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * Represents override properties that can be passed to artifactory with the REST command instead of the descriptor
+ *
+ * @author Dan Feldman
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class BintrayUploadInfoOverride {
+
+    public String subject;
+    public String repoName;
+    public String packageName;
+    public String versionName;
+    public List<String> licenses;
+
+    public BintrayUploadInfoOverride(String subject, String repoName, String packageName,
+                                     String versionName, List<String> licenses) {
+        this.subject = subject;
+        this.repoName = repoName;
+        this.packageName = packageName;
+        this.versionName = versionName;
+        this.licenses = licenses;
+    }
+
+    @JsonIgnore
+    public boolean isValid() {
+        return (StringUtils.isNotBlank(subject) && StringUtils.isNotBlank(repoName)
+                && StringUtils.isNotBlank(packageName) && StringUtils.isNotBlank(versionName)
+                && (licenses != null && !licenses.isEmpty() && StringUtils.isNotBlank(licenses.get(0))));
+    }
+
+    @JsonIgnore
+    public boolean isEmpty() {
+        return subject == null && repoName == null && packageName == null && versionName == null
+                && (licenses == null || licenses.isEmpty());
+    }
+
+}
