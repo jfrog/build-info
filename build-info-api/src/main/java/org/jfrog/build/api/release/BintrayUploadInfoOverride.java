0458437681076a7be5af02bff06ea14d78aa334a
danf <danf@jfrog.com>
2015-02-26 09:10:55 +0200

    BI-194 - Adding "publishToBintray" support to BI

--- build-info-api/src/main/java/org/jfrog/build/api/release/BintrayUploadInfoOverride.java
+++ build-info-api/src/main/java/org/jfrog/build/api/release/BintrayUploadInfoOverride.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 /**
  * Represents override properties that can be passed to artifactory with the REST command instead of the descriptor
+ *
  * @author Dan Feldman
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,28 +18,29 @@ public class BintrayUploadInfoOverride {
     public String repoName;
     public String packageName;
     public String versionName;
-    public List<String> licenses;
+    public List<String> licenses; //Mandatory only for OSS
+    public String vcsUrl;         //Mandatory only for OSS
 
-    public BintrayUploadInfoOverride(String subject, String repoName, String packageName,
-                                     String versionName, List<String> licenses) {
+    public BintrayUploadInfoOverride(String subject, String repoName, String packageName, String versionName,
+                                     List<String> licenses, String vcsUrl) {
         this.subject = subject;
         this.repoName = repoName;
         this.packageName = packageName;
         this.versionName = versionName;
         this.licenses = licenses;
+        this.vcsUrl = vcsUrl;
     }
 
     @JsonIgnore
     public boolean isValid() {
         return (StringUtils.isNotBlank(subject) && StringUtils.isNotBlank(repoName)
-                && StringUtils.isNotBlank(packageName) && StringUtils.isNotBlank(versionName)
-                && (licenses != null && !licenses.isEmpty() && StringUtils.isNotBlank(licenses.get(0))));
+                && StringUtils.isNotBlank(packageName) && StringUtils.isNotBlank(versionName));
     }
 
     @JsonIgnore
     public boolean isEmpty() {
         return subject == null && repoName == null && packageName == null && versionName == null
-                && (licenses == null || licenses.isEmpty());
+                && (licenses == null || licenses.isEmpty()) && vcsUrl == null;
     }
 
 }
