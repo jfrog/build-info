98cd1c3814e7b438200326b790a44d2385567ba1
robin <robin@jfrog.com>
2019-09-04 19:09:28 +0300

    Return empty Vcs when extraction failed

--- build-info-extractor/src/main/java/org/jfrog/build/extractor/clientConfiguration/util/GitUtils.java
+++ build-info-extractor/src/main/java/org/jfrog/build/extractor/clientConfiguration/util/GitUtils.java
@@ -24,13 +24,14 @@ public class GitUtils {
 
     /**
      * Extract Vcs details from .git configuration.
+     * @return Vcs with the details found, empty Vcs if failed to find .git directory
      */
     public static Vcs extractVcs(File workingDir, Log log) throws IOException {
         log.debug("Extracting Vcs details from the .git directory.");
         File dotGit = getDotGit(workingDir);
         if (dotGit == null) {
             log.debug("Could not find the .git directory.");
-            return null;
+            return new Vcs();
         }
         Vcs vcs = new Vcs();
         vcs.setRevision(extractVcsRevision(dotGit, log));
