b2a6417feb3e4e5837db3dddb3a09a7a7bccb3ac
Gary Hale <gary@gradle.com>
2020-01-15 18:01:33 -0500

    Remove unnecessary reflection in ProjectUtils

--- build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/extractor/ProjectUtils.java
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/extractor/ProjectUtils.java
@@ -2,10 +2,8 @@ package org.jfrog.gradle.plugin.artifactory.extractor;
 
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.tasks.TaskState;
 import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask;
 
-import java.lang.reflect.Method;
 import java.util.Set;
 
 public class ProjectUtils {
@@ -18,31 +16,6 @@ public class ProjectUtils {
             return null;
         }
         ArtifactoryTask artifactoryTask = (ArtifactoryTask)tasks.iterator().next();
-        if (ProjectUtils.taskDidWork(artifactoryTask)) {
-            return artifactoryTask;
-        }
-        return null;
-    }
-
-    /**
-     * Determines if the task actually did any work.
-     * This methods wraps Gradle's task.getState().getDidWork().
-     *
-     * @param task The ArtifactoryTask
-     * @return true if the task actually did any work.
-     */
-    static boolean taskDidWork(ArtifactoryTask task) {
-        try {
-            return task.getState().getDidWork();
-        } catch (NoSuchMethodError error) {
-            // Compatibility with older versions of Gradle:
-            try {
-                Method m = task.getClass().getMethod("getState");
-                TaskState state = (TaskState) m.invoke(task);
-                return state.getDidWork();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
+        return artifactoryTask.getState().getDidWork() ? artifactoryTask : null;
     }
 }
