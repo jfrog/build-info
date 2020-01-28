c41a578932028539cc364a01351556d1a8e49554
Eyal Ben Moshe <eyalbenmoshe@jfrog.com>
2020-01-28 18:05:43 +0200

    CR comment.
    Co-Authored-By: Yahav Itzhak <yahavi@users.noreply.github.com>

--- build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/ArtifactoryPlugin.groovy
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/ArtifactoryPlugin.groovy
@@ -29,7 +29,7 @@ class ArtifactoryPlugin extends ArtifactoryPluginBase {
     @Override
     protected DistributeTask createArtifactoryDistributeTask(Project project) {
         def result = project.getTasks().create(DISTRIBUTE_TASK_NAME, DistributeTask.class)
-        result.setDescription('''Distribute build artifacts to Bintray,''')
+        result.setDescription('''Distributes build artifacts to Bintray.'')
         return result
     }
 
