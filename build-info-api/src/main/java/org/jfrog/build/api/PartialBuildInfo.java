449556c4d983998d4b209caf685c1292189babff
Or Gabay <orgab@jfrog.com>
2020-09-23 08:59:37 +0300

    Add vcs & properties to the generated build info

--- build-info-api/src/main/java/org/jfrog/build/api/PartialBuildInfo.java
+++ build-info-api/src/main/java/org/jfrog/build/api/PartialBuildInfo.java
@@ -2,38 +2,54 @@ package org.jfrog.build.api;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
-
-import static org.jfrog.build.api.BuildBean.MODULES;
 
 /**
  * Contains the partial build information along with the repositories it used to download dependencies,
  */
-public class PartialBuildInfo implements Serializable {
+public class PartialBuildInfo extends BaseBuildBean {
     @XStreamAlias(MODULES)
     private List<Module> modules;
     private HashSet<String> ResolutionRepositories;
+    private List<Vcs> vcs;
 
     private PartialBuildInfo() {
 
         ResolutionRepositories = new HashSet();
     }
 
-    public List<Module> getModules() {
-        return modules;
+    public static PartialBuildInfo FromBuildInfo(Build build, HashSet<String> resolverRepositories) {
+        PartialBuildInfo self = new PartialBuildInfo();
+        self.setModules(build.getModules());
+        self.setResolutionRepositories(resolverRepositories);
+        self.setProperties(build.getProperties());
+        self.setVcs(build.getVcs());
+        return self;
     }
 
     public HashSet<String> getResolutionRepositories() {
         return ResolutionRepositories;
     }
 
-    public static PartialBuildInfo FromBuildInfo(Build build, HashSet<String> resolverRepositories) {
-        PartialBuildInfo self = new PartialBuildInfo();
-        self.modules = build.getModules();
-        self.ResolutionRepositories = resolverRepositories;
-        return self;
+    public void setResolutionRepositories(HashSet<String> repositories) {
+        this.ResolutionRepositories = repositories;
+    }
+
+    public List<Vcs> getVcs() {
+        return vcs;
+    }
+
+    public void setVcs(List<Vcs> vcs) {
+        this.vcs = vcs;
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
     }
 
 }
