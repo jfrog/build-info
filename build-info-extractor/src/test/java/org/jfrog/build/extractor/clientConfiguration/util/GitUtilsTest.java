8b404ba15bb0be2e94245743f9e0d4145656e337
barbelity <barb@jfrog.com>
2020-11-18 23:40:34 +0200

    HAP-1389 - rtUpload step support for git submodules

--- build-info-extractor/src/test/java/org/jfrog/build/extractor/clientConfiguration/util/GitUtilsTest.java
+++ build-info-extractor/src/test/java/org/jfrog/build/extractor/clientConfiguration/util/GitUtilsTest.java
@@ -1,5 +1,6 @@
 package org.jfrog.build.extractor.clientConfiguration.util;
 
+import org.apache.commons.io.FileUtils;
 import org.jfrog.build.api.Vcs;
 import org.jfrog.build.api.util.Log;
 import org.jfrog.build.extractor.executor.CommandExecutor;
@@ -10,6 +11,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,5 +53,37 @@ public class GitUtilsTest {
         args.add("--format=%H");
         args.add("HEAD");
         return getGitFieldWithExecutor(execDir, log, args);
+    }
+
+    @Test
+    private void testExtractVcsWithSubmodule() throws IOException, URISyntaxException {
+        String parentDotGitResource = "git_submodule_parent_.git_suffix";
+        String submoduleDotGitResource = "git_submodule_.git_suffix";
+        File testResourcesPath = new File(this.getClass().getResource("/gitutils").toURI()).getCanonicalFile();
+        File parentDotGitFile = new File(testResourcesPath, ".git");
+        String submoduleDotGitFilePath = testResourcesPath.toString() + File.separator + "subDir" + File.separator +
+                "subModule" + File.separator + ".git";
+        File submoduleDotGitFile = new File(submoduleDotGitFilePath);
+
+        try {
+            // Copy the parent folder and create .git.
+            FileUtils.copyDirectory(new File(testResourcesPath, parentDotGitResource), parentDotGitFile);
+
+            // Copy the submodule .git file to the correct path.
+            FileUtils.copyFile(new File(testResourcesPath, submoduleDotGitResource), submoduleDotGitFile);
+
+            // Get VCS info.
+            Log testLog = new TestingLog();
+            Vcs vcs = GitUtils.extractVcs((new File(submoduleDotGitFilePath)).getAbsoluteFile(), testLog);
+
+            // Validate.
+            Assert.assertNotNull(vcs);
+            Assert.assertEquals(vcs.getUrl(), "https://github.com/jfrog/subModule.git");
+            Assert.assertEquals(vcs.getRevision(), "thisIsADummyRevision");
+        } finally {
+            // Cleanup.
+            FileUtils.forceDelete(submoduleDotGitFile);
+            FileUtils.deleteDirectory(parentDotGitFile);
+        }
     }
 }
