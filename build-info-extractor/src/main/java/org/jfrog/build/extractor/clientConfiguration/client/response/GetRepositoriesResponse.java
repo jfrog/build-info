7a4ab98b0f67df5fc4a846e80b534ac26f613b92
Yahav Itzhak <yahavi@users.noreply.github.com>
2021-05-25 14:48:09 +0300

    Fix deserialization error in GetRepositoriesKeys (#501)

---
+++ build-info-extractor/src/main/java/org/jfrog/build/extractor/clientConfiguration/client/response/GetRepositoriesResponse.java
@@ -0,0 +1,13 @@
+package org.jfrog.build.extractor.clientConfiguration.client.response;
+
+public class GetRepositoriesResponse {
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
