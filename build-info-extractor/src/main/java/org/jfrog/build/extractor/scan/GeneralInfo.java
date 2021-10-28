8d9f37d87615b09aa0aa94f76fabf44da19cc510
Yahav Itzhak <yahavi@users.noreply.github.com>
2021-10-28 12:55:29 +0300

    Reduce dependency tree memory consumption (#562)

--- build-info-extractor/src/main/java/org/jfrog/build/extractor/scan/GeneralInfo.java
+++ build-info-extractor/src/main/java/org/jfrog/build/extractor/scan/GeneralInfo.java
@@ -1,5 +1,6 @@
 package org.jfrog.build.extractor.scan;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.Serializable;
@@ -7,17 +8,14 @@ import java.io.Serializable;
 /**
  * @author yahavi
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class GeneralInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private String componentId = "";
-    private String name = "";
-    private String path = "";
     private String pkgType = "";
-    private String groupId = "";
-    private String artifactId = "";
-    private String version = "";
+    private String path = "";
     private String sha1 = "";
 
     @SuppressWarnings("WeakerAccess")
@@ -25,48 +23,43 @@ public class GeneralInfo implements Serializable {
     }
 
     @SuppressWarnings("unused")
-    public GeneralInfo(String componentId, String name, String path, String pkgType) {
+    public GeneralInfo(String componentId, String path, String pkgType) {
         this.componentId = componentId;
-        this.name = name;
         this.path = path;
         this.pkgType = pkgType;
     }
 
     public String getGroupId() {
-        if (StringUtils.isNotBlank(groupId)) {
-            return groupId;
+        int colonMatches = StringUtils.countMatches(componentId, ":");
+        if (colonMatches != 2) {
+            return "";
         }
-        return isValid() ? componentId.substring(0, componentId.indexOf(":")) : "";
+        return componentId.substring(0, componentId.indexOf(":"));
     }
 
     public String getArtifactId() {
-        if (StringUtils.isNotBlank(artifactId)) {
-            return artifactId;
-        }
-        if (!isValid()) {
+        int colonMatches = StringUtils.countMatches(componentId, ":");
+        if (colonMatches < 1 || colonMatches > 2) {
             return "";
         }
         int indexOfColon = componentId.indexOf(":");
-        if (StringUtils.countMatches(componentId, ":") == 1) {
+        if (colonMatches == 1) {
             return componentId.substring(0, indexOfColon);
         }
         return componentId.substring(indexOfColon + 1, componentId.lastIndexOf(":"));
     }
 
     public String getVersion() {
-        if (StringUtils.isNotBlank(version)) {
-            return version;
+        int colonMatches = StringUtils.countMatches(componentId, ":");
+        if (colonMatches < 1 || colonMatches > 2) {
+            return "";
         }
-        return isValid() ? componentId.substring(componentId.lastIndexOf(":") + 1) : "";
+        return componentId.substring(componentId.lastIndexOf(":") + 1);
     }
 
     @SuppressWarnings("unused")
     public String getComponentId() {
         return componentId;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getPath() {
@@ -88,28 +81,8 @@ public class GeneralInfo implements Serializable {
         return this;
     }
 
-    public GeneralInfo name(String name) {
-        this.name = name;
-        return this;
-    }
-
     public GeneralInfo path(String path) {
         this.path = path;
-        return this;
-    }
-
-    public GeneralInfo groupId(String groupId) {
-        this.groupId = groupId;
-        return this;
-    }
-
-    public GeneralInfo artifactId(String artifactId) {
-        this.artifactId = artifactId;
-        return this;
-    }
-
-    public GeneralInfo version(String version) {
-        this.version = version;
         return this;
     }
 
@@ -122,10 +95,5 @@ public class GeneralInfo implements Serializable {
     public GeneralInfo sha1(String sha1) {
         this.sha1 = sha1;
         return this;
-    }
-
-    private boolean isValid() {
-        int colonCount = StringUtils.countMatches(componentId, ":");
-        return colonCount == 1 || colonCount == 2;
     }
 }
