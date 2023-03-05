c382d3faa3423cf8044e7452b48621574fdd0e64
Or Geva <orgab@jfrog.com>
2023-03-05 13:06:26 +0200

    Remove suspected secrets from env vars

--- build-info-extractor/src/main/java/org/jfrog/build/extractor/packageManager/PackageManagerUtils.java
+++ build-info-extractor/src/main/java/org/jfrog/build/extractor/packageManager/PackageManagerUtils.java
@@ -1,5 +1,6 @@
 package org.jfrog.build.extractor.packageManager;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.utils.URIBuilder;
 import org.jfrog.build.extractor.BuildInfoExtractorUtils;
 import org.jfrog.build.extractor.ci.BuildInfo;
@@ -19,6 +20,12 @@ import java.util.Properties;
  * Created by Bar Belity on 12/07/2020.
  */
 public class PackageManagerUtils {
+    private static final String apiKeySecretPrefix = "AKCp8";
+    private static final int apiKeySecretMinimalLength = 73;
+    private static final String referenceTokenSecretPrefix = "cmVmdGtuOjAxOj";
+    private static final int referenceTokenSecretMinimalLength = 64;
+    private static final String accessTokenSecretPrefix = "eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiJ";
+    private static final int accessTokenSecretMinimalLength = 0;
 
     /**
      * Create a new client configuration from the 'buildInfoConfig.propertiesFile' and environment variables.
@@ -102,7 +109,7 @@ public class PackageManagerUtils {
     private static Properties getExcludeIncludeProperties(IncludeExcludePatterns patterns, Properties properties) {
         Properties props = new Properties();
         for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-            if (!isExcludedByKey(patterns, entry) && !isExcludedByValue(patterns, entry)) {
+            if (!isExcludedByKey(patterns, entry) && !containsSuspectedSecrets(entry.getValue().toString())) {
                 props.put(entry.getKey(), entry.getValue());
             }
         }
@@ -113,7 +120,26 @@ public class PackageManagerUtils {
         return PatternMatcher.pathConflicts(entry.getKey().toString(), patterns);
     }
 
-    private static boolean isExcludedByValue(IncludeExcludePatterns patterns, Map.Entry<Object, Object> entry) {
-        return PatternMatcher.isPathExcluded(entry.getValue().toString(), patterns);
+    private static boolean containsSuspectedSecrets(String value) {
+        if (StringUtils.isBlank(value)) {
+            return false;
+        }
+        return containsSuspectedSecret(value, apiKeySecretPrefix, apiKeySecretMinimalLength) ||
+                containsSuspectedSecret(value, referenceTokenSecretPrefix, referenceTokenSecretMinimalLength) ||
+                containsSuspectedSecret(value, accessTokenSecretPrefix, accessTokenSecretMinimalLength);
+    }
+
+    /**
+     * Checks whether the value of a variable contains a suspected secret.
+     * Done by searching for a known constant prefix of the secret and verifying the length of the substring is sufficient to include the expected length of the secret.
+     *
+     * @param variableValue       - string to search in
+     * @param secretPrefix        - secret constant prefix
+     * @param secretMinimalLength - secret minimal expected length
+     * @return whether a secret is suspected
+     */
+    private static boolean containsSuspectedSecret(String variableValue, String secretPrefix, int secretMinimalLength) {
+        int secretIndex = variableValue.indexOf(secretPrefix);
+        return secretIndex > -1 && variableValue.substring(secretIndex).length() >= secretMinimalLength;
     }
 }
